b4eabda7a3d78f63b5de14642daab075916b7b0e
cofffeee <93782833+715494637@users.noreply.github.com>
2023-12-16 01:32:14 +0800

    3

--- config.sh
+++ config.sh
@@ -8,6 +8,7 @@ if [ ! -f ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml ]; then
     mkdir ./Miao-Yunzai/plugins/guoba-plugin/config
     wget -O ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml https://raw.githubusercontent.com/715494637/replit_Qbot/main/gupoba/application.yaml
 fi
-
+echo 适配chatgpt-plugin配置..
 sed -i 's|"serverHost": "[^"]*"|"serverHost": "https://'${REPL_SLUG}'.'${REPL_OWNER}'.repl.co"|' ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json
+echo 适配guoba-plugin配置..
 sed -i "s@host:.*@host: https://${REPL_SLUG}.${REPL_OWNER}.repl.co@" ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml
