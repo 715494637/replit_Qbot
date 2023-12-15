e2488c6256d80cf4f3b65776eb2fe8c160276916
cofffeee <93782833+715494637@users.noreply.github.com>
2023-12-16 01:44:30 +0800

    美化

--- config.sh
+++ config.sh
@@ -1,14 +1,16 @@
-echo 插件配置文件生成/修改
+echo yellow "插件配置文件生成/修改"
 if [ ! -f ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json ]; then
-    echo 生成chatgpt-plugin配置..
+    echo green "生成chatgpt-plugin配置.."
     wget -O ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json https://raw.githubusercontent.com/715494637/replit_Qbot/main/chatgpt/config.json
 fi
 if [ ! -f ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml ]; then
-    echo 生成guoba-plugin配置..
+    echo green "生成guoba-plugin配置.."
     mkdir ./Miao-Yunzai/plugins/guoba-plugin/config
     wget -O ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml https://raw.githubusercontent.com/715494637/replit_Qbot/main/gupoba/application.yaml
 fi
-echo 适配chatgpt-plugin配置..
+
 sed -i 's|"serverHost": "[^"]*"|"serverHost": "https://'${REPL_SLUG}'.'${REPL_OWNER}'.repl.co"|' ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json
-echo 适配guoba-plugin配置..
+echo blue "已适配chatgpt-plugin配置.."
+
 sed -i "s@host:.*@host: https://${REPL_SLUG}.${REPL_OWNER}.repl.co@" ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml
+echo blue "已适配guoba-plugin配置.."
--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
-echo Replit Miao-Yunzai一键部署脚本
-echo By The 一只鬆（yzsong06）
-echo 部署仓库开源地址：https://github.com/yzsong06/Replit-Nodejs-MiaoYunzai
-echo 本项目严禁商用，部署脚本开源于Github
-echo 安装获取依赖文件需求环境
+echo  red "Replit Miao-Yunzai一键部署脚本"
+echo  red "op魔改版"
+echo  red "原仓库开源地址：https://github.com/yzsong06/Replit-Nodejs-MiaoYunzai"
+echo  red "本项目严禁商用，部署脚本开源于Github"
+echo  yellow "安装获取依赖文件需求环境"
 nix-env -iA nixpkgs.nodePackages.pnpm
 nix-env -iA nixpkgs.wget
-echo 从Github拉取Miao-Yunzai文件
+eecho  blue "从Github拉取Miao-Yunzai文件"
 git clone --depth=1 https://github.com/yoimiya-kokomi/Miao-Yunzai.git
 cd Miao-Yunzai 
 git clone --depth=1 https://github.com/yoimiya-kokomi/miao-plugin.git ./plugins/miao-plugin/
 pnpm install
-echo 从Github拉取锅巴插件
+echo  green "从Github拉取锅巴插件"
 git clone --depth=1 https://gitee.com/guoba-yunzai/guoba-plugin.git ./plugins/guoba-plugin/
 pnpm install --filter=guoba-plugin
-echo 从Github拉取chatgpt插件
+echo  yellow "从Github拉取chatgpt插件"
 git clone --depth=1 https://github.com/ikechan8370/chatgpt-plugin.git ./plugins/chatgpt-plugin/
 pnpm install --filter=chatgpt-plugin
 cd ..
 bash <(curl -s https://raw.githubusercontent.com/715494637/replit_Qbot/main/config.sh)
-echo 获取运行指令文件
+echo  blue "获取运行指令文件"
 wget -O .replit https://raw.githubusercontent.com/715494637/replit_Qbot/main/replit/.replit
 wget -O replit.nix https://raw.githubusercontent.com/715494637/replit_Qbot/main/replit/replit.nix
 wget -O main.sh https://raw.githubusercontent.com/715494637/replit_Qbot/main/main.sh
