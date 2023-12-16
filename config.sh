84036fa3c08a2c30d05a0b482089d226bba2508a
cofffeee <93782833+715494637@users.noreply.github.com>
2023-12-16 17:54:33 +0800

    更改

--- config.sh
+++ config.sh
@@ -5,24 +5,24 @@ blue(){ echo -e "\033[36m\033[01m$1\033[0m";}
 
 echo 
 yellow "插件配置文件生成/修改"
-if [ ! -f ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json ]; then
-    echo 
-    green "生成chatgpt-plugin配置.."
-    wget -O ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json https://raw.githubusercontent.com/715494637/replit_Qbot/main/chatgpt/config.json
-fi
-if [ ! -f ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml ]; then
-    echo 
-    green "生成guoba-plugin配置.."
-    mkdir ./Miao-Yunzai/plugins/guoba-plugin/config
-    wget -O ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml https://raw.githubusercontent.com/715494637/replit_Qbot/main/gupoba/application.yaml
-fi
 
-sed -i 's|"serverHost": "[^"]*"|"serverHost": "https://'${REPL_SLUG}'.'${REPL_OWNER}'.repl.co:9000"|' ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json
-sed -i 's|"serverPort": "[^"]*"|"serverPort": "9000"|' ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json
+if [ e ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json ]; then
+    sed -i 's|"serverHost": "[^"]*"|"serverHost": "'${REPL_SLUG}'.'${REPL_OWNER}'.repl.co"|' ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json
+    sed -i 's|"serverPort": "[^"]*"|"serverPort": "9000"|' ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json
+else
+    echo "{{
+  "serverPort": 9000,
+  "serverHost": "${REPL_SLUG}.${REPL_OWNER}.repl.co",
+}}" > ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json
+fi
 echo 
 blue "已适配chatgpt-plugin配置.."
 
-sed -i "s@host:.*@host: https://${REPL_SLUG}.${REPL_OWNER}.repl.co@" ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml
-sed -i "s@port:.*@port: 8099@" ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml
+if [ e ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml ]; then
+    sed -i "s@host:.*@host: https://${REPL_SLUG}.${REPL_OWNER}.repl.co@" ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml
+    sed -i "s@port:.*@port: 8099@" ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml
+fi
+sed -i "s@host:.*@host: https://${REPL_SLUG}.${REPL_OWNER}.repl.co@" ./Miao-Yunzai/plugins/guoba-plugin/defSet/application.yaml
+sed -i "s@port:.*@port: 8099@" ./Miao-Yunzai/plugins/guoba-plugin/defSet/application.yaml
 echo 
 blue "已适配guoba-plugin配置.."
