9fc73b5907de50eaf3dc904f3ebf113e359ddd19
cofffeee <93782833+715494637@users.noreply.github.com>
2023-12-16 18:23:01 +0800

    225

--- install.sh
+++ install.sh
@@ -30,34 +30,12 @@ yellow "从Github拉取chatgpt插件"
 git clone --depth=1 https://github.com/ikechan8370/chatgpt-plugin.git ./plugins/chatgpt-plugin/
 pnpm install --filter=chatgpt-plugin
 cd ..
+bash <(curl -s https://raw.githubusercontent.com/715494637/replit_Qbot/main/config.sh)
 echo  
 blue "获取运行指令文件"
 wget -O .replit https://raw.githubusercontent.com/715494637/replit_Qbot/main/replit/.replit
 wget -O replit.nix https://raw.githubusercontent.com/715494637/replit_Qbot/main/replit/replit.nix
 wget -O main.sh https://raw.githubusercontent.com/715494637/replit_Qbot/main/main.sh
-echo 
-yellow "插件配置文件生成/修改"
-
-if [ e ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json ]; then
-    sed -i 's|"serverHost": "[^"]*"|"serverHost": "'${REPL_SLUG}'.'${REPL_OWNER}'.repl.co"|' ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json
-    sed -i 's|"serverPort": "[^"]*"|"serverPort": "9000"|' ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json
-else
-    echo "{{
-  "serverPort": 9000,
-  "serverHost": "${REPL_SLUG}.${REPL_OWNER}.repl.co",
-}}" > ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json
-fi
-echo 
-blue "已适配chatgpt-plugin配置.."
-
-if [ e ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml ]; then
-    sed -i "s@host:.*@host: https://${REPL_SLUG}.${REPL_OWNER}.repl.co@" ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml
-    sed -i "s@port:.*@port: 8099@" ./Miao-Yunzai/plugins/guoba-plugin/config/application.yaml
-fi
-sed -i "s@host:.*@host: https://${REPL_SLUG}.${REPL_OWNER}.repl.co@" ./Miao-Yunzai/plugins/guoba-plugin/defSet/application.yaml
-sed -i "s@port:.*@port: 8099@" ./Miao-Yunzai/plugins/guoba-plugin/defSet/application.yaml
-echo 
-blue "已适配guoba-plugin配置.."
 echo  
 yellow "部署完成！单击Run或按下键盘上的Ctrl+Enter以启动本项目"
 echo  
