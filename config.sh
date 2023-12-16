4cd6132057deb9bb3c28c9639d288faf32c667c1
cofffeee <93782833+715494637@users.noreply.github.com>
2023-12-16 19:56:41 +0800

    1

--- config.sh
+++ config.sh
@@ -4,11 +4,8 @@ yellow(){ echo -e "\033[33m\033[01m$1\033[0m";}
 blue(){ echo -e "\033[36m\033[01m$1\033[0m";}
 echo 
 yellow "插件配置文件生成"
-if [ ! -f ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json ]; then
-    echo 
-    green "生成chatgpt-plugin配置.."
-    wget -O ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json https://raw.githubusercontent.com/715494637/replit_Qbot/main/chatgpt/config.json
-fi
+green "生成chatgpt-plugin配置.."
+wget -O ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json https://raw.githubusercontent.com/715494637/replit_Qbot/main/chatgpt/config.json
 sed -i 's|"serverHost": "[^"]*"|"serverHost": "'${REPL_SLUG}'.'${REPL_OWNER}'.repl.co:8099"|' ./Miao-Yunzai/plugins/chatgpt-plugin/config/config.json
 echo 
 blue "已适配chatgpt-plugin配置.."
